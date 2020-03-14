65f4cf9d3d56e57d0410d55912c9b26d0e83aed6
fzm <214123975@qq.com>
2020-03-15 03:03:51 +0800

    upd: config

--- README.md
+++ README.md
@@ -1,24 +1,31 @@
 # electron-vue
 
+[vue-cli-plugin-electron-builder](https://nklayman.github.io/vue-cli-plugin-electron-builder)
+
 ## Project setup
-```
+
+```sh
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
-```
+
+```sh
 yarn serve
 ```
 
 ### Compiles and minifies for production
-```
+
+```sh
 yarn build
 ```
 
 ### Lints and fixes files
-```
+
+```sh
 yarn lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- vue.config.js
+++ vue.config.js
@@ -5,9 +5,11 @@ module.exports = {
                 // options placed here will be merged with default configuration and passed to electron-builder
                 publish: ['github'],
                 appId: 'com.example.app',
-                productName: 'electron-vue-example',
+                productName: 'electron-vue',
                 win: {
                     icon: './public/app.ico',
+                    // eslint-disable-next-line no-template-curly-in-string
+                    artifactName: '${productName}_Setup_${version}.${ext}',
                 },
             },
         },
